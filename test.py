f97f28074a2b4e9fb9fd9acf9a16f9013f6c2251
Mizu <40831463+Rexxt@users.noreply.github.com>
2022-11-29 14:23:49 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,16 @@ if len(argv) < 2:
 file_path = argv[1]
 editor = FileEditor(open(file_path, encoding='utf-8').read())
 
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
 with term.fullscreen(), term.hidden_cursor():
+    oy = 0
     while True:
         render = ''
-        for i in range(term.height - 1):
+        for i in range(oy, term.height - 1 + oy):
             if i >= len(editor.text):
+                render += term.bold_grey(f'{len(editor.text)} line(s)')
                 break
             for j in range(len(editor.text[i])):
                 if j > term.width:
@@ -27,12 +32,12 @@ with term.fullscreen(), term.hidden_cursor():
             if editor.cursor[0] == i and editor.cursor[1] >= len(editor.text[i]):
                 render += term.on_white(' ')
             render += '\n'
-        render += term.move_xy(0, term.height - 1) + term.on_blue(file_path) + ' ' + term.bold_blue(f'L{editor.cursor[0] + 1}, C{editor.cursor[1] + 1}')
+        render += term.move_xy(0, term.height - 1) + term.on_blue(file_path) + ' ' + term.bold_blue(f'L{editor.cursor[0] + 1}, C{editor.cursor[1] + 1}') + ' ' + term.green(str(utf8len(str(editor))) + ' B')
 
         print(term.home+term.clear+render, end='')
 
         with term.cbreak():
-            key = term.inkey(timeout=0)
+            key = term.inkey()
             if key.name == 'KEY_ESCAPE':
                 exit()
             elif key.name == u'KEY_LEFT':
@@ -41,11 +46,19 @@ with term.fullscreen(), term.hidden_cursor():
                 editor.move_xy(1, 0)
             elif key.name == u'KEY_DOWN':
                 editor.move_xy(0, 1)
+                if editor.cursor[0] - oy >= term.height - 1:
+                    oy += 1
             elif key.name == u'KEY_UP':
                 editor.move_xy(0, -1)
+                if editor.cursor[0] - oy < 0:
+                    oy -= 1
             elif key.name == u'KEY_ENTER':
                 editor.write('\n')
+                if editor.cursor[0] - oy >= term.height - 1:
+                    oy += 1
             elif key.name == u'KEY_BACKSPACE':
+                if editor.cursor[0] - oy < 0:
+                    oy -= 1
                 editor.backspace()
             else:
                 editor.write(str(key))
